21c6bf46360cc034ced298e34ce7f9e0878ce767
Ivan Veselov <veselov.iva@gmail.com>
2016-03-19 15:35:58 +0300

    Added auxiliary classes Point and PointSupport for linearize function. Function quantized returns matrix of angles now.

--- segment_decomposition.py
+++ segment_decomposition.py
@@ -15,15 +15,39 @@ def get_orientation_map(img):
 
 
 def quantized(orientation_map, quantization_channels):
-    quantized_map = numpy.ndarray(orientation_map.shape, int)
+    quantized_map = numpy.ndarray(orientation_map.shape, float)
+    unit_angle = 360.0 / quantization_channels
+    
     for i, row in enumerate(orientation_map):
         for j, angle in enumerate(row):
-            quantized_map[i][j] = angle / 360.0 * quantization_channels
+            units_number = int(round(angle / 360.0 * quantization_channels)) % quantization_channels
+            quantized_map[i][j] = unit_angle * units_number
     
     return quantized_map
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+
+class PointSupport:
+    def __init__(self, point):
+        self.point = point
+        self.support = 0
+
+
+def find_support(edge_map, orientation_map, x, y):
+    pass
+
+
 def linearize(edge_map, orientation_map, quantization_channels):
+    orientation_map = quantized(orientation_map, quantization_channels)
     pass
 
 
