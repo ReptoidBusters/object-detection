7a47302e5efad87b501bc7a90d7009ec4ffeaa84
ArtyomLobanov <lobanov98@yandex.ru>
2016-04-17 14:31:46 +0300

    minor fixes

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -1,3 +1,9 @@
+from . loading import load_object
+from . geometry import Object3D, Point
+
 __author__ = 'Artyom_Lobanov'
 
-__all__ = ["geometry", "loading"]
+__all__ = ['Object3D', 'Point', 'load_object']
+
+
+
