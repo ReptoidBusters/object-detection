8bdd214072f71413ac5b8b91862c54158695de42
Ivan Veselov <veselov.iva@gmail.com>
2016-04-17 20:36:41 +0300

    PEP8 formating.

--- segment_decomposition.py
+++ segment_decomposition.py
@@ -4,37 +4,38 @@ import collections
 import math
 import random
 
+
 def get_edge_map(img):
     return cv2.Canny(img, 100, 200)  # Need to choose thresholds properly
 
 
 def get_orientation_map(img):
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     # Need to pay attention to the arguments
     xder = cv2.Sobel(gray_img, cv2.CV_32F, 1, 0)
     yder = cv2.Sobel(gray_img, cv2.CV_32F, 0, 1)
-    
+
     orientation_map = cv2.phase(xder, yder)
     for i, row in enumerate(orientation_map):
         for j, angle in enumerate(row):
             orientation_map[i][j] += math.pi / 2
             orientation_map[i][j] %= math.pi
-    
+
     return orientation_map
 
 
 def quantized(orientation_map, quantization_channels):
     quantized_map = numpy.ndarray(orientation_map.shape, float)
     unit_angle = math.pi / quantization_channels
-    
+
     for i, row in enumerate(orientation_map):
         for j, angle in enumerate(row):
             units_number = int(round(angle / math.pi * quantization_channels))
             units_number %= quantization_channels
-            
+
             quantized_map[i][j] = unit_angle * units_number
-    
+
     return quantized_map
 
 
@@ -44,25 +45,26 @@ class PointSupport:
         self.orientation = orientation
         self.support = 0
         self.list = [point]
-    
+
     def angle_deviation(self, point):
-        line_vector = numpy.array([math.cos(self.orientation), math.sin(self.orientation)])
+        line_vector = numpy.array([math.cos(self.orientation),
+                                   math.sin(self.orientation)])
         point_vector = point - self.point
-        
+
         cross_product = numpy.cross(line_vector, point_vector)
         dot_product = numpy.dot(line_vector, point_vector)
         deviation = math.atan2(cross_product, dot_product) + 2 * math.pi
         return deviation % math.pi
-    
+
     def residual(self, point):
         distance = numpy.linalg.norm(self.point - point)
         res = distance * math.sin(self.angle_deviation(point))
         return res
-    
+
     def add_point(self, point):
         if self.residual(point) > 2:  # Need to choose thresholds properly
             return False
-        
+
         self.support += 1
         self.list.append(point)
         return True
@@ -74,31 +76,34 @@ class LineSegment:
         self.orientation = support.orientation
         self.ytype = self._determine_type()
         self._calculate_bounds(support)
-    
+
     def _determine_type(self):
         return math.pi / 4 <= self.orientation <= 3 * math.pi / 4
-        
+
     def _calculate_bounds(self, support):
         if self.ytype:
             axis = 1
         else:
             axis = 0
-        
+
         self.left_bound = 0
         self.right_bound = 0
         for point in support.list:
-            self.left_bound = min(self.left_bound, point[axis] - self.point[axis])
-            self.right_bound = max(self.right_bound, point[axis] - self.point[axis])
-    
+            candidate = point[axis] - self.point[axis]
+            self.left_bound = min(self.left_bound, candidate)
+            self.right_bound = max(self.right_bound, candidate)
+
     def get_points_list(self):
         result = []
         if not self.ytype:
             for x in range(self.left_bound, self.right_bound):
-                result.append(numpy.array([x, math.tan(self.orientation) * x]) + self.point)
+                point = numpy.array([x, math.tan(self.orientation) * x])
+                result.append(point + self.point)
         else:
             for y in range(self.left_bound, self.right_bound):
-                result.append(numpy.array([1 / math.tan(self.orientation) * y, y]) + self.point)
-        
+                point = numpy.array([1 / math.tan(self.orientation) * y, y])
+                result.append(point + self.point)
+
         return result
 
 
@@ -107,16 +112,16 @@ def find_support(edge_map, point_orientation, initial_point):
                   numpy.array([0, -1]), numpy.array([-1, 0]),
                   numpy.array([1, 1]), numpy.array([1, -1]),
                   numpy.array([-1, -1]), numpy.array([-1, 1])]
-    
+
     used_point = numpy.ndarray(edge_map.shape, bool)
     used_point.fill(False)
-    
+
     support = PointSupport(initial_point, point_orientation)
-    
+
     queue = collections.deque()
     queue.append(initial_point)
     used_point[initial_point[1]][initial_point[0]] = True
-    
+
     while len(queue) > 0:
         point = queue.popleft()
         if not support.add_point(point):
@@ -124,16 +129,16 @@ def find_support(edge_map, point_orientation, initial_point):
 
         for d in directions:
             next_point = point + d
-            
+
             if (not (0 <= next_point[1] < len(edge_map) and
-                     0 <= next_point[0] < len(edge_map[0])) or
-                edge_map[next_point[1]][next_point[0]] == 0 or
-                used_point[next_point[1]][next_point[0]]):
+                0 <= next_point[0] < len(edge_map[0])) or
+                    edge_map[next_point[1]][next_point[0]] == 0 or
+                    used_point[next_point[1]][next_point[0]]):
                 continue
-            
+
             queue.append(next_point)
             used_point[next_point[1]][next_point[0]] = True
-    
+
     return support
 
 
@@ -145,30 +150,32 @@ def linearize(edge_map, orientation_map, quantization_channels):
         for j in range(0, len(edge_map[i])):
             if edge_map[i][j] == 255:
                 points_list.append(numpy.array([j, i]))
-    
+
     segments_list = []
     while len(points_list):
         support = PointSupport(numpy.array([0, 0]), 0.0)
         candidates = 10
-        
+
         while len(points_list) and candidates:
             index = random.randint(0, len(points_list) - 1)
             point = points_list[index]
             if edge_map[point[1]][point[0]] == 0:
                 points_list.pop(index)
                 continue
-                
+
             candidates -= 1
-            newsupport = find_support(edge_map, orientation_map[point[1]][point[0]], point)
+            newsupport = find_support(edge_map,
+                                      orientation_map[point[1]][point[0]],
+                                      point)
+
             if newsupport.support >= support.support:
                 support = newsupport
-        
+
         if support.support < 5:
             break
-        
+
         segments_list.append(LineSegment(support))
         for support_point in support.list:
             edge_map[support_point[1]][support_point[0]] = 0
 
     return segments_list
-    
