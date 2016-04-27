11c0bc5b44c5e3a1ef8ca567b024f58af893971e
ArtyomLobanov <lobanov98@yandex.ru>
2016-04-27 16:53:48 +0400

    Some geometric primitives and algorithms were realised.

--- geometry/geometry.py
+++ geometry/geometry.py
@@ -1,28 +1,112 @@
+import numpy.linalg as lin
+import numpy as np
+from geometry.geometry3D import ConvexPolygon
+from geometry.geometry3D import Ray
 __author__ = 'Artyom_Lobanov'
 
 
-class Point:  # pylint: disable=invalid-name
-
+class Point:
     def __init__(self, x, y, z, w=1):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.w = w
+        self.vector = np.array([x, y, z, w])
+
+    @staticmethod
+    def key_code(key):
+        return {"x": 0, "y": 1, "z": 2, "w": 3}[key]
+
+    def __setattr__(self, key, value):
+        if key == "vector":
+            self.__dict__[key] = value
+        else:
+            self.__dict__["vector"][self.key_code(key)] = value
+
+    def __getattr__(self, key):
+        return self.vector[Point.key_code(key)]
 
     def __repr__(self):
         lst = [self.x, self.y, self.z, self.w]
         lst = [str(i) for i in lst]
         return "(" + ", ".join(lst) + ")"
 
+    def normalize(self):
+        if self.vector[3] == 0:
+            raise Exception("infinite point")
+        self.vector = self.vector / self.vector[3]
+
+    def as_vector(self):
+        return self.vector
+
+    def to_cartesian_coordinates(self):
+        return self.vector[:3] / self.vector[3]
+
 
 class Object3D:
 
     def __init__(self, faces, points):
         self.faces = faces  # list of tuples which define faces
-        self.points = points
+        self.point_store = PointStore(points)
 
     def __repr__(self):
-        points = [p.__repr__() for p in self.points]
+        points = [p.__repr__() for p in self.point_store.to_points_array()]
         faces = [f.__repr__() for f in self.faces]
-        return "Object3D[points = " + ",\n".join(points) + \
-               "]\n[faces = " + ",\n".join(faces) + "]"
+        return "Object3D[points = " + ",\n".join(points) + "]\n[faces = " + ",\n".join(faces) + "]"
+
+    def intersect(self, p1, p2):
+        p1 = p1.to_cartesian_coordinates()
+        p2 = p2.to_cartesian_coordinates()
+        ray = Ray(p1, p2)
+        res = None
+        for face in self.faces:
+            points = [self.point_store.get_point(i) for i in face]
+            points = [p.to_cartesian_coordinates() for p in points]
+            polygon = ConvexPolygon(points)
+            point = polygon.intersect(ray)
+            if point is None:
+                continue
+            if res is None:
+                res = point
+            if lin.norm(res - p1) > lin.norm(point - p1):
+                res = point
+        return res
+
+
+class PointStore:
+    def __init__(self, points):
+        self.array = np.empty((len(points), 4), dtype=np.double)
+        self.size = len(points)
+        for i in range(len(points)):
+            self.array[i, :] = points[i].as_vector()
+
+    def transform(self, index, transformation):
+        self.array[index, :] *= transformation.transpose()
+
+    def transform_all(self, transformation):
+        self.array = self.array.dot(transformation.transpose())
+
+    def normalize_all(self):
+        for i in range(4):
+            self.array[:, i] /= self.array[:, 3]
+
+    def get_point(self, i):
+        return Point(*self.array[i, :])
+
+    def get_vector(self, i):
+        return self.array[i, :]
+
+    def to_points_array(self):
+        return [self.get_point(i) for i in range(self.size)]
+
+
+
+"""
+p4.normalize()
+print(p1)
+ps = PointStore([p1, p2, p3, p4])
+ps.normalize_all()
+print(ps.to_points_array())
+
+p1, p2, p3 = map(Point, (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 2 , 1))
+p4 = Point(9, 18, 12, 3)
+points = [p1, p2, p3, p4]
+p = map(lambda x: x.normalize(), points)
+print(*points)
+"""
--- geometry/geometry3D.py
+++ geometry/geometry3D.py
@@ -1,1 +1,121 @@
+import numpy as np
+import numpy.linalg as lin
+__author__ = 'Artyom_Lobanov'
 
+"""
+    This package implements some geometric object and algorithms.
+    It works in 3D Cartesian coordinate system.
+    It's expected that numpy.array will be used as point
+"""
+
+
+class Plane:
+
+    """
+        Define plane by normal and distance to origin.
+    """
+    def __init__(self, normal, d):
+        self.normal = normal
+        self.d = d
+
+    def contain(self, point):
+        return abs(np.dot(self.normal, point) + self.d) < 0.0001
+
+    def intersect(self, ray):  # (begin + t * d) * k = 0
+        """
+        Return common point of this plane and the ray.
+        If the ray lies in plane or if they are parallel, method return None.
+        """
+        tmp = self.normal.dot(ray.d)
+        if not tmp:
+            return None
+        distance = -(self.normal.dot(ray.begin) + self.d) / tmp  # begin * k + D = -t * d * k
+        return ray.get_point(distance)
+
+    @staticmethod
+    def create_plane(point1, point2, point3):
+        """
+        Return plane, that contain all that points.
+        If point isn't different, raise Exception
+        """
+        normal = np.cross(point2 - point1, point3 - point1)
+        length = lin.norm(normal)
+        if not length:  # normal = 0
+            raise RuntimeError("Can't create plane: All points should be different")
+        normal /= length  # now normal is normalized
+        d = - np.dot(normal, point1)
+        return Plane(normal, d)
+
+    def __repr__(self):
+        normal_str = "(" + ", ".join([str(x) for x in self.normal]) + ")"
+        return "Plane[Normal = " + normal_str + "; D = " + str(self.d) + "]"
+
+
+class Ray:
+
+    def __init__(self, begin, another):
+        self.begin = np.copy(begin)
+        self.d = another - begin
+
+    def get_point(self, distance):
+        if distance < 0:
+            return None
+        return self.begin + self.d * distance
+
+
+def is_collinear(vector_a, vector_b):
+    len_a = lin.norm(vector_a)
+    len_b = lin.norm(vector_b)
+    if not len_a or not len_b:
+        return True
+    diff = vector_a / len_a - vector_b / len_b
+    return lin.norm(diff) < 0.0001
+
+
+class ConvexPolygon:
+
+    def __init__(self, points):
+        self.points = points
+        self.plane = Plane.create_plane(points[0], points[1], points[2])
+        print(points)
+
+    def intersect(self, ray):
+        """
+        Return common point of polygon and ray.
+        If there is many such point, return None.
+        If there is no such point, return None.
+        """
+        suspect = self.plane.intersect(ray)
+        if suspect is None:
+            return None
+        if self.contain(suspect):
+            return suspect
+        return None
+
+    def contain(self, point):
+        """
+        Check if polygon contain that point.
+        """
+        points = self.points
+        orientation = np.cross(points[2] - points[0], points[1] - points[0])
+        for p1, p2 in zip(points, np.roll(points, -1, axis=0)):
+            res = np.cross(point - p1, p2 - p1)
+            if not is_collinear(res, orientation):
+                return False
+        return True
+
+
+
+"""
+p1, p2, p3 = map(Point, (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 2 , 1))
+p4 = Point(9, 18, 12, 3)
+p4.normalize()
+print(p1)
+ps = PointStore([p1, p2, p3, p4])
+ps.normalize_all()
+print(ps.to_points_array())
+
+"""
+
+p = Plane.create_plane(np.array([0, 1, 0]), np.array([0, 0, 1]), np.array([1, 0, 0]))
+print(p)
