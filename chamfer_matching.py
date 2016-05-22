3d7e70a9c0b2b4e10dc0295d441e2e10c48500cc
Ivan Veselov <veselov.iva@gmail.com>
2016-05-22 15:12:32 +0300

    Now Matcher creates all the necessary structures using segment_decomposition.

--- chamfer_matching.py
+++ chamfer_matching.py
@@ -2,9 +2,11 @@ from segment_decomposition import*
 
 class Matcher:
     def __init__(self):
-        self.hysteresis_threshold1 = 100;
-        self.hysteresis_threshold2 = 200;
+        self.hysteresis_threshold1 = 100
+        self.hysteresis_threshold2 = 200
         self.quantization_channels = 16
+        self.img_pixel_residual = 1.0
+        self.pattern_pixel_residual = 1.0
         
     def set_image(self, img):
         self.img_edge_map = get_edge_map(img,
@@ -14,3 +16,20 @@ class Matcher:
         self.img_orientation_map = get_orientation_map(img)
         self.img_orientation_map = quantized(self.img_orientation_map,
                                              self.quantization_channels)
+                                             
+        self.img_segments_list = linearize(self.img_edge_map,
+                                           self.img_orientation_map,
+                                           self.quantization_channels,
+                                           self.img_pixel_residual)
+                                           
+    def set_pattern(self, pattern):
+        self.pattern_edge_map = convert_to_binary(pattern)
+        self.pattern_orientation_map = guess_quantized_orientation_map(
+                                                  self.pattern_edge_map,
+                                                  self.quantization_channels,
+                                                  self.pattern_pixel_residual)
+                                                  
+        self.pattern_segments_list = linearize(self.pattern_edge_map,
+                                               self.pattern_orientation_map,
+                                               self.quantization_channels,
+                                               self.pattern_pixel_residual)
