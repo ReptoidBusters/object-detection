d2e4f665e29a531600f150ca2ec8e9a7cbc4006f
Ilya <scorpioni4@yandex.ru>
2016-05-29 19:55:06 +0300

    Finished callbacks

--- feature-detection/callbacks.py
+++ feature-detection/callbacks.py
@@ -1,5 +1,6 @@
 import cv2
 import matcher
+import base.frame as frame
 
 
 def detect(img):
@@ -25,14 +26,28 @@ def processKeyFrame(img, ms, obj):
     '''
     keypoints = detect(img)
     res = []
+    img_size = (len(img), len(img[0]))
     for keyp in keypoints:
-        p3d = ARTEM_DAVI(obj, ms, keyp[0])
+        p3d = obj.get_original(ms[0], ms[1], ms[2], img_size[0], img_size[1],
+                               (keyp[0][0], img_size[1] - keyp[0][1]))
         res.append((keyp[1], p3d))
 
     return res
 
 
-def processImage(img, ms, keyframepoints):
+def GLtoCV3d(point):
+    return (point[0], -point[1], -point[2])
+
+
+def GLtoCV2d(point, img_size):
+    return (point[0], img_size[0] - point[1])
+
+
+def processImage(keyframes, obj, img):
+    keyframe = next(iter(dict.values))
+    keyframepoints = processKeyFrame(keyframe.image, (keyframe.camera_position,
+                                     keyframe.internal_camera_parametrs,
+                                     keyframe.object_position), obj)
     keypoints = detect(img)
 
     desc1 = [pnt[0] for pnt in keyframepoints]
@@ -41,9 +56,20 @@ def processImage(img, ms, keyframepoints):
 
     obj_points = []
     img_points = []
+    img_size = (len(img), len(img[0]))
     for mt in matches:
-        obj_points.append(keyframepoints[mt[0]][1])
-        img_points.append(keypoints[mt[1]][0])
+        obj_points.append(GLtoCV3d(keyframepoints[mt[0]][1]))
+        img_points.append(GLtoCV2d(keypoints[mt[1]][0], img_size))
+
+    ms = [[0] * 3] * 3
+    for i in range(3):
+        for j in range(3):
+            ms[i][j] = keyframe.internal_camera_parametrs[i][j]
 
     found, rvec, tvec = cv2.solvePnP(obj_points, img_points, ms, None)
-    
+
+    object_position = frame.Position(tvec, rvec)
+
+    return frame.KeyFrame(img, keyframe.camera_position,
+                          keyframe.internal_camera_parametrs,
+                          object_position)
