886f37ea607680008982cdc57eaba71f024dc259
ArtyomLobanov <lobanov98@yandex.ru>
2016-05-31 01:20:49 +0400

    Interface of object matching

--- object_matching.py
+++ object_matching.py
@@ -1,2 +1,33 @@
-def match_object():
+import numpy as np
+
+
+def match_object(object3d, frames, image):
+
+    def find_edges(frame):
+        # i have no Open CV, so i don't really sure in this line
+        height, width, trash = frame.image.shape
+        # Please, check that image_size = [width, height]
+        image_size = np.array([width, height])
+        return object3d.get_border(frame.object_position,
+                                   frame.camera_position,
+                                   frame.internal_camera_parameters,
+                                   image_size)
+
+    edges = dict()
+
+    for label, key_frame in frames:
+        edges[label] = find_edges(key_frame)
+
+    return do_something(frames, edges, image)
+
+
+def do_something(frames, edges, image):
+    """
+    frames - dict: labels->frame
+    edges - dict: labels->edges for frame with equal label
+    """
     pass
+
+
+
+
